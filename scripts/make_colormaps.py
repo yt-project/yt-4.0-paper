0dc34cc449b1516572f95ad96f7ca297889f93a2
Matthew Turk <matthewturk@gmail.com>
2023-01-31 15:44:27 -0600

    expand plotting stuff

---
+++ scripts/make_colormaps.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cmyt
+import yt
+
+colormaps = [ "algae", "arbre", "dusk", "kelp", "octarine", "pastel",
+             "pixel_blue", "pixel_green", "pixel_red", "xray"]
+
+x = np.mgrid[0.0:1.0:512j]
+ones = np.ones(64).astype("f8")
+
+for colormap in colormaps:
+    cmap = getattr(cmyt, colormap)
+    v = cmap(x)[None, :, :] * ones[:, None, None]
+    yt.write_bitmap(v, f"content/images/colormaps/{colormap}.png")
