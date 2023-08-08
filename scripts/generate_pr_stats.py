5e956ba07a7a35f32be29bdfa10bc8884ebdaa56
Matthew Turk <matthewturk@gmail.com>
2023-08-08 12:25:42 -0500

    Starting pull request stats

---
+++ scripts/generate_pr_stats.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import json
+pull_requests = json.load(open("pr.json"))
+# We're going to use json_normalize, but it has fewer options, so we have to do
+# more manipulation after we load it.
+df = pd.json_normalize(pull_requests)
+
+# We only want the ones that are closed ...
+df = df[(~df["closedAt"].isna()) & (df["state"] == "MERGED")]
+
+df[["createdAt", "closedAt"]] = df[["createdAt", "closedAt"]].apply(pd.to_datetime)
+df["duration"] = (df["closedAt"] - df["createdAt"]).dt.total_seconds()
+
+pr_count = df.groupby("author.login").size()
+
+df["author"] = df["author.name"]
+df["author"].fillna(df["author.login"])
+df["author"].fillna(df["author.id"])
+
+df.to_csv("content/images/pr_stats.csv", columns = ["createdAt", "closedAt", "author", "number", "duration"])
